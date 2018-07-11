70e920c54e58249e5204dc780423fd787b5e95c8
ddionisio <garapata@gmail.com>
2018-07-11 16:10:52 -0400

    project setup

---
+++ Assets/LoL/Scripts/LoLPlaySoundButtonClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoLPlaySoundButtonClick : LoLPlaySound {
+    public Button button;
+
+    void OnDestroy() {
+        if(button)
+            button.onClick.RemoveListener(OnClick);
+    }
+
+    void Awake() {
+        if(!button)
+            button = GetComponent<Button>();
+
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick() {
+        Play();
+    }
+}
---
+++ Assets/Scripts/DebugControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Make sure to create this in Resources with name: debugControl
+/// </summary>
+[CreateAssetMenu(fileName = "debugControl", menuName = "Game/Debug Control")]
+public class DebugControl : M8.SingletonScriptableObject<DebugControl> {
+    public int levelIndex;
+    public bool collectionsUnlocked;
+}
