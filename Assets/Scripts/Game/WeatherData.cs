8d3cc9df44016c6e0f939c79553dccde2c9fbca2
ddionisio <garapata@gmail.com>
2018-07-22 17:40:08 -0400

    prep game ui with placeholder

--- Assets/Scripts/Game/WeatherData.cs
+++ Assets/Scripts/Game/WeatherData.cs
@@ -2,15 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "weather", menuName = "Game/Weather")]
-public class WeatherData : ScriptableObject {
-    [Header("Display")]
-    public Sprite icon;
-
-    [M8.Localize]
-    public string titleRef; //short detail, ex: Mostly Sunny, T-Storms
-    [M8.Localize]
-    public string detailRef; //description of the weather
+[System.Serializable]
+public class WeatherData {
+    public WeatherType weather;
 
     [Header("Data")]
     public int uvLevel;
---
+++ Assets/Scripts/Game/WeatherType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "weather", menuName = "Game/Weather")]
+public class WeatherType : ScriptableObject {
+    [Header("Display")]
+    public Sprite icon;
+
+    [M8.Localize]
+    public string titleRef; //short detail, ex: Mostly Sunny, T-Storms
+    [M8.Localize]
+    public string detailRef; //description of the weather
+}
