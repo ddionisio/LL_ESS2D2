a1ffe771ff8cc53962fe37a4fedda2a3c5b2ae6f
ddionisio <garapata@gmail.com>
2018-08-05 18:46:32 -0400

    other scenes prep

---
+++ Assets/LoL/Scripts/Game/CutsceneController.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour {
+    [System.Serializable]
+    public struct Data {
+        public string name;
+        public GameObject root;
+        public M8.Animator.Animate animator;
+        public string takeEnter;        
+        public string takeExit;
+    }
+
+    [Header("Music")]
+    public string playMusicPath; //if not empty, play this music
+    public bool startMusicPlaylist = true; //make sure playMusicPath is empty
+
+    [Header("Modals")]
+    public string modalDialog; //use for opening dialog via OpenDialog, set to null to use generic dialog.
+    
+    [Header("Animation")]
+    public M8.Animator.Animate animator;
+    public string takeStart; //prep background
+    public string takeInteractEnter; //'next' button enter
+    public string takeInteractExit; //'next' button exit
+
+    [Header("Pages")]
+    public Data[] pages;
+
+    public bool playOnStart = true;
+    public bool progressOnEnd = true;
+
+    private int mCurPageInd;
+    private bool mIsDialogOpen;
+    private bool mIsInteractActive;
+
+    public void OpenDialog(string nameTextRef, string dialogTextRef) {
+        if(!string.IsNullOrEmpty(modalDialog))
+            ModalDialog.Open(modalDialog, nameTextRef, dialogTextRef, NextPage);
+        else
+            ModalDialog.Open(nameTextRef, dialogTextRef, NextPage);
+
+        mIsDialogOpen = true;
+    }
+
+    public void OpenDialogPortrait(Sprite portrait, string nameTextRef, string dialogTextRef) {
+        if(!string.IsNullOrEmpty(modalDialog))
+            ModalDialog.OpenApplyPortrait(modalDialog, portrait, nameTextRef, dialogTextRef, NextPage);
+        else
+            ModalDialog.OpenApplyPortrait(portrait, nameTextRef, dialogTextRef, NextPage);
+
+        mIsDialogOpen = true;
+    }
+
+    public void CloseDialog() {
+        if(!string.IsNullOrEmpty(modalDialog)) {
+            M8.UIModal.Manager.instance.ModalCloseUpTo(modalDialog, true);
+        }
+        else
+            M8.UIModal.Manager.instance.ModalCloseUpTo(ModalDialog.modalNameGeneric, true);
+
+        mIsDialogOpen = false;
+    }
+
+    public void Play() {
+        StopAllCoroutines();
+
+        HideAllPages();
+
+        mIsDialogOpen = false;
+        mIsInteractActive = false;
+
+        StartCoroutine(DoPlay());
+    }
+
+    public void NextPage() {
+        StartCoroutine(DoGoNextPage());
+    }
+
+    void Awake() {
+        //ensure UIRoot exists
+        if(!UIRoot.isInstantiated)
+            UIRoot.Reinstantiate();
+
+        HideAllPages();
+    }
+
+    IEnumerator Start() {
+        if(playOnStart) {
+            while(M8.SceneManager.instance.isLoading)
+                yield return null;
+
+            StartCoroutine(DoPlay());
+        }
+    }
+
+    void HideAllPages() {
+        for(int i = 0; i < pages.Length; i++) {
+            if(pages[i].root)
+                pages[i].root.SetActive(false);
+        }
+    }
+
+    // Use this for initialization
+    IEnumerator DoPlay() {
+        //setup initial display
+        animator.ResetTake(animator.GetTakeIndex(takeStart));
+        animator.ResetTake(animator.GetTakeIndex(takeInteractEnter));
+                
+        animator.Play(takeStart);
+        while(animator.isPlaying)
+            yield return null;
+
+        //music
+        if(!string.IsNullOrEmpty(playMusicPath)) {
+            LoLMusicPlaylist.instance.Stop(); //ensure playlist is stopped
+
+            LoLManager.instance.PlaySound(playMusicPath, true, true);
+        }
+        else if(startMusicPlaylist)
+            LoLMusicPlaylist.instance.Play();
+
+        //start up the first page
+        mCurPageInd = 0;
+        ShowCurrentPage();
+    }
+
+    void ShowCurrentPage() {
+        StartCoroutine(DoShowCurrentPage());
+    }
+
+    IEnumerator DoShowCurrentPage() {
+        if(mCurPageInd < pages.Length) {
+            var page = pages[mCurPageInd];
+
+            if(page.root)
+                page.root.SetActive(true);
+
+            if(page.animator && !string.IsNullOrEmpty(page.takeEnter)) {
+                page.animator.Play(page.takeEnter);
+                while(page.animator.isPlaying)
+                    yield return null;
+            }
+        }
+
+        if(!mIsDialogOpen) {
+            animator.Play(takeInteractEnter);
+            mIsInteractActive = true;
+        }
+    }
+
+    IEnumerator DoGoNextPage() {
+        if(mIsInteractActive) {
+            animator.Play(takeInteractExit);
+            mIsInteractActive = false;
+        }
+
+        bool isLastPage = pages.Length == 0 || mCurPageInd == pages.Length - 1;
+
+        if(mCurPageInd < pages.Length) {
+            var page = pages[mCurPageInd];
+
+            if(page.animator && !string.IsNullOrEmpty(page.takeExit)) {
+                page.animator.Play(page.takeExit);
+                while(page.animator.isPlaying)
+                    yield return null;
+            }
+
+            //only deactivate if it's the last page or the next page has a different root
+            if(isLastPage || page.root != pages[mCurPageInd + 1].root)
+                page.root.SetActive(false);
+        }
+
+        if(!isLastPage) {
+            mCurPageInd++;
+            ShowCurrentPage();
+        }
+        else if(progressOnEnd) { //proceed
+            M8.UIModal.Manager.instance.ModalCloseAll();
+            GameData.instance.Progress();
+        }
+    }
+}
---
+++ Assets/LoL/Scripts/UI/Modals/ModalDialog.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModalDialog : M8.UIModal.Controller, M8.UIModal.Interface.IPush, M8.UIModal.Interface.IPop {
+    public const string modalNameGeneric = "dialog";
+
+    public const string parmPortraitSprite = "p";
+    public const string parmNameTextRef = "n";
+    public const string parmDialogTextRef = "t";
+    public const string parmNextCallback = "c";
+
+    public Image portraitImage;
+    public Text nameLabel;
+    public Text textLabel;
+
+    public bool isPortraitResize;
+    public bool isCloseOnNext;
+    public bool isTextSpeechAuto = true;
+
+    [Header("Signals")]
+    public M8.Signal signalNext; //when the next button is pressed.
+    
+    private static M8.GenericParams mParms = new M8.GenericParams();
+
+    private string mDialogTextRef;
+    private System.Action mNextCallback;
+
+    /// <summary>
+    /// Open generic dialog: modalNameGeneric
+    /// </summary>
+    public static void Open(string nameTextRef, string dialogTextRef, System.Action nextCallback) {
+        Open(modalNameGeneric, nameTextRef, dialogTextRef, nextCallback);
+    }
+
+    public static void Open(string modalName, string nameTextRef, string dialogTextRef, System.Action nextCallback) {
+        //check to see if there's one already opened
+        var uiMgr = M8.UIModal.Manager.instance;
+                
+        if(uiMgr.ModalIsInStack(modalName)) {
+            var dlg = uiMgr.ModalGetController<ModalDialog>(modalName);
+
+            dlg.mNextCallback = nextCallback;
+
+            dlg.SetupTextContent(nameTextRef, dialogTextRef);
+
+            if(dlg.isTextSpeechAuto)
+                dlg.PlayDialogSpeech();
+        }
+        else {
+            mParms[parmNameTextRef] = nameTextRef;
+            mParms[parmDialogTextRef] = dialogTextRef;
+            mParms[parmNextCallback] = nextCallback;
+
+            uiMgr.ModalOpen(modalName, mParms);
+        }
+    }
+
+    /// <summary>
+    /// Open generic dialog, and apply portrait: modalNameGeneric. set portrait to null to hide portrait
+    /// </summary>
+    public static void OpenApplyPortrait(Sprite portrait, string nameTextRef, string dialogTextRef, System.Action nextCallback) {
+        OpenApplyPortrait(modalNameGeneric, portrait, nameTextRef, dialogTextRef, nextCallback);
+    }
+
+    /// <summary>
+    /// Open dialog and apply portrait. set portrait to null to hide portrait
+    /// </summary>
+    public static void OpenApplyPortrait(string modalName, Sprite portrait, string nameTextRef, string dialogTextRef, System.Action nextCallback) {
+        //check to see if there's one already opened
+        var uiMgr = M8.UIModal.Manager.instance;
+
+        if(uiMgr.ModalIsInStack(modalName)) {
+            var dlg = uiMgr.ModalGetController<ModalDialog>(modalName);
+
+            dlg.mNextCallback = nextCallback;
+
+            dlg.SetupPortraitTextContent(portrait, nameTextRef, dialogTextRef);
+
+            if(dlg.isTextSpeechAuto)
+                dlg.PlayDialogSpeech();
+        }
+        else {
+            mParms[parmPortraitSprite] = portrait;
+            mParms[parmNameTextRef] = nameTextRef;
+            mParms[parmDialogTextRef] = dialogTextRef;
+            mParms[parmNextCallback] = nextCallback;
+
+            uiMgr.ModalOpen(modalName, mParms);
+        }
+    }
+
+    public void Next() {
+        if(isCloseOnNext)
+            Close();
+
+        if(mNextCallback != null)
+            mNextCallback();
+
+        if(signalNext != null)
+            signalNext.Invoke();
+    }
+
+    public void PlayDialogSpeech() {
+        if(LoLManager.isInstantiated && !string.IsNullOrEmpty(mDialogTextRef))
+            LoLManager.instance.SpeakText(mDialogTextRef);
+    }
+        
+    public override void SetActive(bool aActive) {
+        base.SetActive(aActive);
+
+        //play text speech if auto
+        if(isTextSpeechAuto)
+            PlayDialogSpeech();
+    }
+
+    void M8.UIModal.Interface.IPop.Pop() {
+        mNextCallback = null;
+    }
+
+    void M8.UIModal.Interface.IPush.Push(M8.GenericParams parms) {
+        if(parms != null) {
+            if(parms.ContainsKey(parmPortraitSprite))
+                SetupPortraitTextContent(parms.GetValue<Sprite>(parmPortraitSprite), parms.GetValue<string>(parmNameTextRef), parms.GetValue<string>(parmDialogTextRef));
+            else
+                SetupTextContent(parms.GetValue<string>(parmNameTextRef), parms.GetValue<string>(parmDialogTextRef));
+        }
+    }
+
+    private void SetupPortraitTextContent(Sprite portrait, string nameTextRef, string dialogTextRef) {        
+        if(portraitImage) {
+            if(portrait) {
+                portraitImage.gameObject.SetActive(true);
+
+                portraitImage.sprite = portrait;
+                if(isPortraitResize)
+                    portraitImage.SetNativeSize();
+            }
+            else
+                portraitImage.gameObject.SetActive(false);
+        }
+
+        SetupTextContent(nameTextRef, dialogTextRef);
+    }
+
+    private void SetupTextContent(string nameTextRef, string dialogTextRef) {
+        //setup other stuff?
+
+        mDialogTextRef = dialogTextRef;
+
+        if(nameLabel) {
+            nameLabel.text = !string.IsNullOrEmpty(nameTextRef) ? M8.Localize.Get(nameTextRef) : "";
+        }
+
+        if(textLabel) {
+            textLabel.text = !string.IsNullOrEmpty(dialogTextRef) ? M8.Localize.Get(dialogTextRef) : "";
+        }
+    }
+}
