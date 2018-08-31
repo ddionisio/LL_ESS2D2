5cbca7ab6915683dacb65819681f4293e3f50dbc
ddionisio <garapata@gmail.com>
2018-08-30 21:09:15 -0400

    level 1 background complete

---
+++ Assets/Scripts/Game/WeatherCycleAnimatorActive.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherCycleAnimatorActive : MonoBehaviour {
+    public WeatherType weather;
+    public GameObject rootGO;
+    public M8.Animator.Animate animator;
+    public string take;
+
+    private bool mIsActive = false;
+
+    void OnDestroy() {
+        if(GameController.isInstantiated && GameController.instance.weatherCycle) {
+            GameController.instance.weatherCycle.weatherBeginCallback -= OnWeatherBegin;
+            GameController.instance.weatherCycle.cycleEndCallback -= OnCycleEnd;
+        }
+    }
+
+    void Awake() {
+        GameController.instance.weatherCycle.weatherBeginCallback += OnWeatherBegin;
+        GameController.instance.weatherCycle.cycleEndCallback += OnCycleEnd;
+
+        if(rootGO) rootGO.SetActive(false);
+    }
+
+    void OnWeatherBegin() {
+        var curWeather = GameController.instance.weatherCycle.curWeather;
+
+        bool active = curWeather.type == weather;
+        if(mIsActive != active) {
+            mIsActive = active;
+
+            if(rootGO) rootGO.SetActive(mIsActive);
+
+            if(mIsActive)
+                animator.Play(take);
+        }
+    }
+
+    void OnCycleEnd() {
+        if(mIsActive) {
+            mIsActive = false;
+            if(rootGO) rootGO.SetActive(false);
+        }
+    }
+}
